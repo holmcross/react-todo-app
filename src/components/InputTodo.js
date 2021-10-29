0b80114437f8f0438dfd3525b26cfca05d1fa02b
holmcross <holmcross@users.noreply.github.com>
2021-10-28 17:36:46 -0700

    commit

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react"
 
 class InputTodo extends Component {
-
-    state = {
-        title: ""
-    };
-
-    onChange = e => {
-        this.setState({
-          title: e.target.value
-        });
-    };
-
-
+  state = {
+    title: "",
+  }
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
   render() {
     return (
       <form>
-        <input type="text" placeholder="Add Todo..." value={this.state.title} 
-            onChange={this.onChange}
+        <input
+          type="text"
+          placeholder="Add todo..."
+          value={this.state.title}
+          name="title"
+          onChange={this.onChange}
         />
         <button>Submit</button>
       </form>
