79f000d2c27e682142ed572f08c8fd1e4a0c33da
holmcross <holmcross@users.noreply.github.com>
2021-11-02 16:01:00 -0700

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from "react"
 
-const headerStyle = {
-  padding: "20px 0",
-  lineHeight: "1.5em",
-}
-
 const Header = () => {
+  // style can be placed into a variable
+  const headerStyle = {
+    padding: "20px 0",
+    lineHeight: "1.5em",
+  }
+
   return (
     <header style={headerStyle}>
       <h1
+        //inline styling
         style={{
           fontSize: "6rem",
           fontWeight: "600",
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,20 +1,25 @@
 import React from "react"
+import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
 
   //the this.props.todo.id arguement is sent back to TodoContainer to identify which component was
   //  clicked
   render(){
-    return <li>
-      <input
-        type="checkbox"
-        checked={this.props.todo.completed}
-        //on change, run the handleChange function passed from TodoContainer
-        onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-      />
-      <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>Delete</button>
-      {this.props.todo.title}
-    </li>
+    return (
+      <li className={styles.item}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={this.props.todo.completed}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        />
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
+        {this.props.todo.title}
+      </li>
+    )
   }
 }
 
